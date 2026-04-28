845839e829a5b949d4ddae0cf044ea4c699b690b
Ing3dreco <ing3dreco@gmail.com>
2026-04-28 13:09:08 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import { db } from "./supabase.js";
 async function iniciarApp(){
   document.getElementById('view-login').style.display = 'none';
   document.getElementById('view-app').style.display = 'block';

047c8f7c3a03779e79986486f6b59fa7525dfba4
Ing3dreco <ing3dreco@gmail.com>
2026-04-28 12:53:17 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,10 @@
 const SUPABASE_URL = 'https://nlebxviivoqbunkkejlc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sZWJ4dmlpdm9xYnVua2tlamxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODMwMzYsImV4cCI6MjA5Mjk1OTAzNn0.E3OUvhjw2_LIIIq-oaak8kgDzR1JXpKwedUD4SvEwMM';
 
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-let currentUser = null;
-let clienteActivo = null;
+let user = null;
+let clientes = [];
+let activo = null;
+
+const META = 10;
